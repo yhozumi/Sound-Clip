8911b9bd4c7b232dfc23d29c5995d125a8690fda
Suke Hozumi <yusuke.hozumi@icloud.com>
2016-07-16 14:30:08 -0400

    Initial Commit

---
+++ Sound Clip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Clip
+//
+//  Created by Suke Hozumi on 7/16/16.
+//  Copyright © 2016 Suke Hozumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
